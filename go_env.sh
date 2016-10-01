83ee72303db63138ebf6c469ba749fa5c3b2da45
GUO Yixuan <culu.gyx@gmail.com>
2016-10-01 17:16:06 -0400

    try cross

--- go_env.sh
+++ go_env.sh
@@ -1,2 +1,6 @@
 # debian go packages in /usr/share/gocode
 export GOPATH="$PWD:/usr/share/gocode/:/usr/lib/go-1.6";
+# for cross compiling
+#export GOOS=linux
+#export GOARCH=amd64
+#export GOARCH=arm
