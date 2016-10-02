8af8f9c70524cc87792f7e0d50d5095c447541d0
GUO Yixuan <culu.gyx@gmail.com>
2016-10-02 12:48:15 -0400

    goroot

--- go_env.sh
+++ go_env.sh
@@ -1,6 +1,7 @@
 # debian go packages in /usr/share/gocode
-export GOROOT="/home/pkg/go"
-export GOPATH="$PWD:/usr/share/gocode/";
+#export GOROOT="/home/pkg/go"
+export GOROOT=$(go env GOROOT)
+export GOPATH="$PWD:/usr/share/gocode/:$GOROOT";
 # for cross compiling
 export GOOS=linux
 export GOARCH=amd64
